e5480847e76e252522d666ff24ee14733d53d3cc
adriano lima <adriano.limaa@outlook.com>
2025-02-28 21:39:01 -0300

    Adiciona template com bootstrap

--- meusite/home/views.py
+++ meusite/home/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
-def index(request):
-    return HttpResponse("Essa é a primeira view")
+def home(request):
+    return render(request, 'home.html')
